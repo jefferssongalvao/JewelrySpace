7f5c2899a08411cd4dcf7a0f3c4d686de60cc671
rubemkalebe <rubemkalebe@hotmail.com>
2014-06-03 21:17:41 -0300

    Implementação de recurso para destacar a joia que foi selecionada (fa… …

--- Tela.cpp
+++ Tela.cpp
@@ -491,11 +491,13 @@
 				                    p2.y = tmp.x;
 			                	} else {
 			                		//Ignora selecao
+			                		//Tira o destaque da joia que tinha sido selecionada
 			                		contrastItem(p1.x, p1.y, matriz[p1.x][p1.y].elemento);
 			        				apply_surface(p1.x, p1.y, gems, screen);
                 					SDL_UpdateRect(screen, matriz[p1.x][p1.y].celula.x, matriz[p1.x][p1.y].celula.y, matriz[p1.x][p1.y].celula.w, matriz[p1.x][p1.y].celula.h);
 			                		p1.x = tmp.y;
 					                p1.y = tmp.x;
+					                //Destaca a nova joia selecionada
 					                contrastItem(p1.x, p1.y, matriz[p1.x][p1.y].elemento);
 			                    	apply_surface(p1.x, p1.y, gems_on, screen);
                 					SDL_UpdateRect(screen, matriz[p1.x][p1.y].celula.x, matriz[p1.x][p1.y].celula.y, matriz[p1.x][p1.y].celula.w, matriz[p1.x][p1.y].celula.h);
